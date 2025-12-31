aa34892c8560933bf313274a09481dba7720d9df
Harsshitha S <harsshitha2005@gmail.com>
2025-12-31 17:17:25 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          cyan: '#00d4ff',
+          purple: '#8b5cf6',
+        }
+      },
+      fontFamily: {
+        orbitron: ['Orbitron', 'monospace'],
+        jetbrains: ['JetBrains Mono', 'monospace'],
+      },
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
+    },
+  },
+  plugins: [],
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    static: './public',
+    hot: true
+  }
+};
